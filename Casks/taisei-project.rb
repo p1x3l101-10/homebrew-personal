37034d9751eb10b514c0b0c2f807b33b994c883e
Pixel <scott.blatt.0b10@gmail.com>
2024-08-18 16:13:11 -0500

    add taisei-project Cask

---
+++ Casks/taisei-project.rb
@@ -0,0 +1,13 @@
+cask "taisei-project" do
+  version "1.4.1"
+  sha256 "eaae180f2013a4a3f140290da803a566c78206887a584f29d497ac27aa1b5cb2"
+
+  url "https://github.com/taisei-project/taisei/releases/download/v#{version}/Taisei-#{version}-universal.dmg"
+  name "Taisei Project"
+  desc "A free and open-source Touhou Project fangame "
+  homepage "https://taisei-project.org/"
+
+  app "Taisei.app"
+
+  zap trash: ""
+end

59aa80321047b840fd6788f9170532ac14bf4adb
Pixel <scott.blatt.0b10@gmail.com>
2024-08-18 16:30:12 -0500

    remove comments

--- Formula/z/zulu@8.rb
+++ Formula/z/zulu@8.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ZuluAT8 < Formula
   desc "java 8 for aarch64"
   homepage "https://www.azul.com/"

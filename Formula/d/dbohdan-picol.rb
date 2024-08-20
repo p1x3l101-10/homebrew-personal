8f7afc383da624034fa87bb3cc6f4e81e72d85f7
Pixel <scott.blatt.0b10@gmail.com>
2024-08-20 13:52:56 -0500

    change to git

--- Formula/d/dbohdan-picol.rb
+++ Formula/d/dbohdan-picol.rb
@@ -2,7 +2,7 @@ class DbohdanPicol < Formula
   desc "Tiny Tcl interpreter"
   homepage "https://chiselapp.com/user/dbohdan/repository/picol/"
   license "BSD2"
-  head "https://chiselapp.com/user/dbohdan/repository/picol", using: :fossil
+  head "https://github.com/dbohdan/picol.git", using: :git
 
   def install
     system "make", "all"

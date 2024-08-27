3d2c4567a9a1ff6a2b71ad537dc6bb79979cab92
Pixel <scott.blatt.0b10@gmail.com>
2024-08-27 11:53:14 -0500

    restructure install

--- Formula/m/mindustry.rb
+++ Formula/m/mindustry.rb
@@ -36,27 +36,39 @@ class Mindustry < Formula
   end
 
   def install
-    if (build.head?) or (build.without? "prebuilt")
+    # Build jar if there is a need to
+    if ((build.head?) or (build.without? "prebuilt")) and (build.without? "app")
       system "./gradlew", "desktop:dist"
-      libexec.install "desktop/build/libs/Mindustry.jar"
-    else
-      libexec.install "Mindustry.jar"
     end
-    bin.write_jar_script "#{libexec}/Mindustry.jar", "mindustry", java_version: "17"
 
     if (not (build.without? "app")) and not ((build.without? "prebuilt") or (build.head?))
       system "mkdir", "-p", "#{libexec}/Mindustry.app/Contents/MacOS", "#{libexec}/Mindustry.app/Contents/Resources"
-      system "unzip", "#{libexec}/Mindustry.jar", "-d", buildpath
+      system "unzip", "Mindustry.jar", "-d", buildpath
       system "mv", "icons/icon.icns", "#{libexec}/Mindustry.app/Contents/Resources/icons.icns"
-      system "cp", "-RL", "#{HOMEBREW_PREFIX}/opt/openjdk@17/libexec/openjdk.jdk", "#{libexec}/Mindustry.app/Contents/Resources/openjdk.jdk"
-      system "cp", "#{libexec}/Mindustry.jar", "#{libexec}/Mindustry.app/Contents/Resources/Mindustry.jar"
+      system "ln", "-s", "#{HOMEBREW_PREFIX}/opt/openjdk@17/libexec/openjdk.jdk", "#{libexec}/Mindustry.app/Contents/Resources/openjdk.jdk" # Use softlink to slim
+      system "mv", "Mindustry.jar", "#{libexec}/Mindustry.app/Contents/Resources/Mindustry.jar"
       # Need base64 here because ruby hates this stuff
+      ## Info.plist -- Reason: The symbols XML uses conflict with the symbols Ruby uses, the amount of escaping needed would defeat the purpose of making this human-readable
       system "sh", "-c", "echo 'PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPCFET0NUWVBFIHBsaXN0IFBVQkxJQyAiLS8vQXBwbGUgQ29tcHV0ZXIvL0RURCBQTElTVCAxLjAvL0VOIiAiaHR0cDovL3d3dy5hcHBsZS5jb20vRFREcy9Qcm9wZXJ0eUxpc3QtMS4wLmR0ZCI+CjxwbGlzdCB2ZXJzaW9uPSIxLjAiPgo8ZGljdD4KICA8a2V5PkNGQnVuZGxlR2V0SW5mb1N0cmluZzwva2V5PgogIDxzdHJpbmc+TWluZHVzdHJ5PC9zdHJpbmc+CiAgPGtleT5DRkJ1bmRsZUV4ZWN1dGFibGU8L2tleT4KICA8c3RyaW5nPk1pbmR1c3RyeTwvc3RyaW5nPgogIDxrZXk+Q0ZCdW5kbGVJZGVudGlmaWVyPC9rZXk+CiAgPHN0cmluZz5taW5kdXN0cnkubWFjPC9zdHJpbmc+CiAgPGtleT5DRkJ1bmRsZU5hbWU8L2tleT4KICA8c3RyaW5nPk1pbmR1c3RyeTwvc3RyaW5nPgogIDxrZXk+Q0ZCdW5kbGVJY29uRmlsZTwva2V5PgogIDxzdHJpbmc+aWNvbnMuaWNuczwvc3RyaW5nPgogIDxrZXk+Q0ZCdW5kbGVTaG9ydFZlcnNpb25TdHJpbmc8L2tleT4KICA8c3RyaW5nPjEuMDwvc3RyaW5nPgogIDxrZXk+Q0ZCdW5kbGVJbmZvRGljdGlvbmFyeVZlcnNpb248L2tleT4KICA8c3RyaW5nPjYuMDwvc3RyaW5nPgogIDxrZXk+Q0ZCdW5kbGVQYWNrYWdlVHlwZTwva2V5PgogIDxzdHJpbmc+QVBQTDwvc3RyaW5nPgogIDxrZXk+SUZNYWpvclZlcnNpb248L2tleT4KICA8aW50ZWdlcj4wPC9pbnRlZ2VyPgogIDxrZXk+SUZNaW5vclZlcnNpb248L2tleT4KICA8aW50ZWdlcj4xPC9pbnRlZ2VyPgogIDxrZXk+TlNIaWdoUmVzb2x1dGlvbkNhcGFibGU8L2tleT4KICA8dHJ1ZS8+CjwvZGljdD4KPC9wbGlzdD4=' | base64 -d > #{libexec}/Mindustry.app/Contents/Info.plist"
+      ## Runner script -- Reason: The string manipulation that this script uses conflicts with the symbols Ruby uses
       system "sh", "-c", "echo 'IyEvYmluL2Jhc2gKZXhwb3J0IEFQUF9MT0NBVElPTj0iJChjZCAiJChkaXJuYW1lICIkMCIpLy4uLy4uIjsgcHdkKSIKZWNobyAiQVBQX0xPQ0FUSU9OIGlzICR7QVBQX0xPQ0FUSU9OfSIKZXhwb3J0ICJKQVZBX0hPTUUiPSIke0pBVkFfSE9NRTotIiR7QVBQX0xPQ0FUSU9OfS9Db250ZW50cy9SZXNvdXJjZXMvb3Blbmpkay5qZGsvQ29udGVudHMvSG9tZSJ9IgplY2hvICJKQVZBX0hPTUUgaXMgJHtKQVZBX0hPTUV9IgplY2hvICJQcmludGluZyBKYXZhIHZlcnNpb24gaW5mby4uLiIKZXZhbCAiJHtKQVZBX0hPTUV9L2Jpbi9qYXZhIC12ZXJzaW9uIgpleGVjICIke0pBVkFfSE9NRX0vYmluL2phdmEiICItWHN0YXJ0T25GaXJzdFRocmVhZCIgIi1EaHR0cHMucHJvdG9jb2xzPVRMU3YxLjIsVExTdjEuMSxUTFN2MSIgIi1YWDorU2hvd0NvZGVEZXRhaWxzSW5FeGNlcHRpb25NZXNzYWdlcyIgLWphciAiJHtBUFBfTE9DQVRJT059L0NvbnRlbnRzL1Jlc291cmNlcy9NaW5kdXN0cnkuamFyIiAiJEAiCg==' | base64 -d > #{libexec}/Mindustry.app/Contents/MacOS/Mindustry"
       system "chmod", "+x", "#{libexec}/Mindustry.app/Contents/MacOS/Mindustry"
+
+      # Link running script to app bundle, no need to duplicate files
+      bin.install_symlink libexec/"Mindustry.app/Contents/MacOS/Mindustry" => "mindustry"
     elsif (not (build.without? "app")) and ((build.without? "prebuilt") or (build.head?))
       system "./gradlew", "desktop:packr"
       libexec.install Dir ["build/packr/output/Mindustry.app"]
+      # Link running script to app bundle, no need to duplicate files
+      bin.install_symlink libexec/"Mindustry.app/Contents/MacOS/Mindustry" => "mindustry"
+    # Steps for building w/out app bundle
+    elsif build.without? "app"
+      if (build.head?) or (build.without? "prebuilt")
+        libexec.install "desktop/build/libs/Mindustry.jar"
+      else
+        libexec.install "Mindustry.jar"
+      end
+      bin.write_jar_script "#{libexec}/Mindustry.jar", "mindustry", java_version: "17"
     end
   end
 

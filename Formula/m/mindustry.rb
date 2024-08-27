bfa742f775e2b5083725474226919bc88ee48d28
Pixel <scott.blatt.0b10@gmail.com>
2024-08-27 12:03:09 -0500

    WRONG ARG, WRONG ARG

--- Formula/m/mindustry.rb
+++ Formula/m/mindustry.rb
@@ -35,7 +35,7 @@ class Mindustry < Formula
         NOTE: The app bundle utilizes symlinkt to slim down the size and deduplicate resources
           This also allows the bundle to automagically update the jre when openjdk@17 is also updated
           To make the app portable, copy the bundle while dereferenceing it like this:
-            cp -rR #{libexec}/Mindustry.app ~/Applications/Mindustry.app
+            cp -rL #{libexec}/Mindustry.app ~/Applications/Mindustry.app
       EOS
     end
   end

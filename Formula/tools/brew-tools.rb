044dca46b61203592089c686bb71861d6a89958f
Pixel <scott.blatt.0b10@gmail.com>
2024-09-19 08:44:49 -0500

    updates

--- Formula/tools/brew-tools.rb
+++ Formula/tools/brew-tools.rb
@@ -1,10 +1,10 @@
 class BrewTools < Formula
   desc "Homebrew extensions"
   homepage "https://github.com/p1x3l101-10/brew-tools"
-  version "1.0"
+  version "1.1"
   url "https://github.com/p1x3l101-10/brew-tools.git",
     tag: "v#{version}",
-    revision: "7c7ca09cb5e449bdac0d85b15a71867f1ce38747"
+    revision: "a0c1086ca03817e742b42afc32416bafb19225ec"
 
 
   def install

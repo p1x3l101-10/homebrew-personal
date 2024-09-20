d318e30a8a04ffaef9606aee5934556e9a22b3b2
Pixel <scott.blatt.0b10@gmail.com>
2024-09-20 14:17:23 -0500

    add head version

--- Formula/tools/brew-tools.rb
+++ Formula/tools/brew-tools.rb
@@ -5,6 +5,7 @@ class BrewTools < Formula
   url "https://github.com/p1x3l101-10/brew-tools.git",
     tag: "v#{version}",
     revision: "875f21d6f990eabd193b5e01f2284a346b348882"
+  head "https://github.com/p1x3l101-10/brew-tools.git"
   
   depends_on "cmake" => :build
 

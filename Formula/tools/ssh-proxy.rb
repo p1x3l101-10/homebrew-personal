aa2dc0ca080a5dd728832623d988e4240c77b6c8
Pixel <scott.blatt.0b10@gmail.com>
2024-11-01 15:25:43 -0500

    fix head

--- Formula/tools/ssh-proxy.rb
+++ Formula/tools/ssh-proxy.rb
@@ -5,7 +5,8 @@ class SshProxy < Formula
   url "https://github.com/p1x3l101-10/ssh-proxy.git",
     tag: "v#{version}"
   sha256 "823c1a9a237c0dd117bc54cfda59e3202524d70fafbb2b37816116615ef69bbf"
-  HEAD "https://github.com/p1x3l101-10/ssh-proxy.git"
+  head "https://github.com/p1x3l101-10/ssh-proxy.git",
+    branch: "main"
 
   depends_on "cmake" => :build
 

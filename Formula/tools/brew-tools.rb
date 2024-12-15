c4b86d9213a536daa8d8b0161b6d9111d5b03905
Pixel <scott.blatt.0b10@gmail.com>
2024-12-14 22:33:46 -0600

    bump

--- Formula/tools/brew-tools.rb
+++ Formula/tools/brew-tools.rb
@@ -1,7 +1,7 @@
 class BrewTools < Formula
   desc "Homebrew extensions"
   homepage "https://github.com/p1x3l101-10/brew-tools"
-  version "2.9"
+  version "2.10"
   url "https://github.com/p1x3l101-10/brew-tools.git",
     tag: "v#{version}"
   head "https://github.com/p1x3l101-10/brew-tools.git",

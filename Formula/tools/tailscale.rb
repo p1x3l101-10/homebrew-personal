6648494bbdb6dc99ffce6036becd1bcc2dd8efa1
Pixel <scott.blatt.0b10@gmail.com>
2025-02-04 11:56:28 -0600

    bump

---
+++ Formula/tools/tailscale.rb
@@ -0,0 +1,49 @@
+class Tailscale < Formula
+  desc "Easiest, most secure way to use WireGuard and 2FA"
+  homepage "https://tailscale.com"
+  url "https://github.com/tailscale/tailscale.git",
+      tag:      "v1.78.3",
+      revision: "1b41fdeddb6598b35ba15cc6b07740e0cc0e8411"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "go" => :build
+
+  def install
+    vars = Utils.safe_popen_read("./build_dist.sh", "shellvars")
+    ldflags = %W[
+      -s -w
+      -X tailscale.com/version.longStamp=#{vars.match(/VERSION_LONG="(.*)"/)[1]}
+      -X tailscale.com/version.shortStamp=#{vars.match(/VERSION_SHORT="(.*)"/)[1]}
+      -X tailscale.com/version.gitCommitStamp=#{vars.match(/VERSION_GIT_HASH="(.*)"/)[1]}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/tailscale"
+    system "go", "build", *std_go_args(ldflags:, output: bin/"tailscaled"), "./cmd/tailscaled"
+
+    generate_completions_from_executable(bin/"tailscale", "completion")
+  end
+
+  service do
+    run [ opt_bin/"tailscaled", "--tun=userspace-networking", "--socket=#{var}/run/tailscaled.socket" ]
+    keep_alive true
+    log_path var/"log/tailscaled.log"
+    error_log_path var/"log/tailscaled.log"
+  end
+
+  test do
+    version_text = shell_output("#{bin}/tailscale version")
+    assert_match version.to_s, version_text
+    assert_match(/commit: [a-f0-9]{40}/, version_text)
+
+    fork do
+      system bin/"tailscaled", "-tun=userspace-networking", "-socket=#{testpath}/tailscaled.socket"
+    end
+
+    sleep 2
+    assert_match "Logged out.", shell_output("#{bin}/tailscale --socket=#{testpath}/tailscaled.socket status", 1)
+  end
+end

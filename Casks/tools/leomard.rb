bf543120f3aec9401fe26a026b358f203c34833a
Pixel <scott.blatt.0b10@gmail.com>
2025-03-20 16:15:44 -0500

    fix details

--- Casks/tools/leomard.rb
+++ Casks/tools/leomard.rb
@@ -3,9 +3,9 @@ cask "leomard" do
   sha256 "546b355852f27cdfe6100641bb5571f61674657927c57d8477770755ebbb0791"
 
   url "https://github.com/Athlon007/Leomard/releases/download/#{version}/Leomard.dmg"
-  name "layendanimat"
-  desc "Anime Viewing app for Desktop"
-  homepage "https://github.com/Layendan/Layendanimator"
+  name "Leomard"
+  desc "A Lemmy client for macOS "
+  homepage "https://github.com/Athlon007/Leomard"
 
   app "Leomard.app"
 

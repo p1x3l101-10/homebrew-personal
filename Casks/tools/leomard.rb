bee9501cc2c8ad2018ea4ea142c8b2bd8ed61120
Pixel <scott.blatt.0b10@gmail.com>
2025-03-20 16:13:40 -0500

    add checksum

--- Casks/tools/leomard.rb
+++ Casks/tools/leomard.rb
@@ -1,6 +1,6 @@
 cask "leomard" do
   version "0.6"
-  sha256 ""
+  sha256 "546b355852f27cdfe6100641bb5571f61674657927c57d8477770755ebbb0791"
 
   url "https://github.com/Athlon007/Leomard/releases/download/#{version}/Leomard.dmg"
   name "layendanimat"

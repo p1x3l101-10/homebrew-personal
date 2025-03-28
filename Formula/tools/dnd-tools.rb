7c7b4f06c1c8e309d3d614d75742b345ab63d215
Pixel <scott.blatt.0b10@gmail.com>
2025-03-28 13:31:24 -0500

    add dnd-tools

---
+++ Formula/tools/dnd-tools.rb
@@ -0,0 +1,15 @@
+class DndTools < Formula
+  desc "Set of interactive command line tools for Dungeons and Dragons 5th Edition"
+  homepage "https://gitlab.com/savagezen/dnd-tools"
+  license "AGPL-3.0"
+  version "2022-07-31"
+  url "https://gitlab.com/savagezen/dnd-tools.git", revision: "faa09f6c01de423c4e592126e24ad929f8a9f164"
+
+  head "https://gitlab.com/savagezen/dnd-tools.git", branch: "main"
+
+  depends_on "python@3.13"
+
+  def install
+    system "python", "setup.py", "install", "--root=#{prefix}"
+  end
+end

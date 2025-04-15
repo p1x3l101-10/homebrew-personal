8035e3327794e7e4532a98b6981fa4548b7611c1
Pixel <scott.blatt.0b10@gmail.com>
2025-04-15 12:25:10 -0500

    version bump

--- Formula/tools/ssh-proxy.rb
+++ Formula/tools/ssh-proxy.rb
@@ -1,7 +1,7 @@
 class SshProxy < Formula
   desc "SSH Proxy Daemon"
   homepage "https://github.com/p1x3l101-10/ssh-proxy"
-  version "1.6.1"
+  version "1.6.2"
   url "https://github.com/p1x3l101-10/ssh-proxy.git",
     tag: "v#{version}"
   head "https://github.com/p1x3l101-10/ssh-proxy.git",

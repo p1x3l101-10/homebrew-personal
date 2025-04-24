5c2d77d2d019a652db83236c9db33a26f388daaf
Pixel <scott.blatt.0b10@gmail.com>
2025-04-24 18:04:43 -0500

    bump

--- Formula/tools/ssh-proxy@2.rb
+++ Formula/tools/ssh-proxy@2.rb
@@ -1,7 +1,7 @@
 class SshProxyAT2 < Formula
   desc "SSH Proxy Daemon"
   homepage "https://github.com/p1x3l101-10/ssh-proxy"
-  version "2.0.1"
+  version "2.0.2"
   head "https://github.com/p1x3l101-10/ssh-proxy.git",
     branch: "main"
   url "https://github.com/p1x3l101-10/ssh-proxy.git",

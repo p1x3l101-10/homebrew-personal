fff2f41d7a25c18084360ae1e288d6fcae5081c2
Pixel <scott.blatt.0b10@gmail.com>
2025-04-25 11:07:00 -0500

    bump

--- Formula/tools/socks5-relay.rb
+++ Formula/tools/socks5-relay.rb
@@ -1,7 +1,7 @@
 class Socks5Relay < Formula
   desc "SOCKS5 Relay Proxy Daemon"
   homepage "https://github.com/p1x3l101-10/ssh-proxy"
-  version "2.0.5"
+  version "2.0.6"
   head "https://github.com/p1x3l101-10/ssh-proxy.git",
     branch: "main"
   url "https://github.com/p1x3l101-10/ssh-proxy.git",
